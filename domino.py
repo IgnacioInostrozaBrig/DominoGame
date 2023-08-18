48f32247122d9575a6b56eb4fa16555e6238cff6
Milcao <mjanas@miuandes.cl>
2023-08-18 01:41:35 -0400

    feat(Domino): Added method to swap tile sides

--- domino.py
+++ domino.py
@@ -3,5 +3,8 @@ class Domino:
         self.side1 = side1
         self.side2 = side2
     
+    def swap_sides(self):
+        self.side1, self.side2 = self.side2, self.side1
+        
     def __str__(self):
         return f'[{self.side1};{self.side2}]'
